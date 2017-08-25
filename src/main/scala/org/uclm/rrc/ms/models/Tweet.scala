563e011f1fe9c2ea233ebd55223519769e4782e3
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2017-08-25 18:00:30 +0200

    [IMP] tweet schema completed via case class

--- src/main/scala/org/uclm/rrc/ms/models/Tweet.scala
+++ src/main/scala/org/uclm/rrc/ms/models/Tweet.scala
@@ -1,8 +1,7 @@
 package org.uclm.rrc.ms.models
 
 case class Tweet (created_at: String, id: Long, text: String, source: String, truncated: Boolean,
-                  coordinates: Coordinate, retweet_count: Long, favourite_count: Long, lang: String,
-                  timestamp_ms: Long, user: User)
+                  retweet_count: Long, favorite_count: Long, lang: String, timestamp_ms: String)
 
 case class User(id: Long, name: String, verifie: Boolean, followers_count: Long, friend_count: Long,
                 favourites_count: Long, created_at: String, lang: String, time_zone: String)

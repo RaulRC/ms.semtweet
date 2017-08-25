f3ebdadd96be75871beebbd0973a00a5fdba07af
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2017-08-25 18:21:27 +0200

    [IMP] user well defined

--- src/main/scala/org/uclm/rrc/ms/models/Tweet.scala
+++ src/main/scala/org/uclm/rrc/ms/models/Tweet.scala
@@ -1,10 +1,11 @@
 package org.uclm.rrc.ms.models
 
-case class Tweet (created_at: String, id: Long, text: String, source: String, truncated: Boolean,
-                  retweet_count: Long, favorite_count: Long, lang: String, timestamp_ms: String)
+case class User(id: Long, name: String, verified: Boolean, followers_count: Long, friends_count: Long,
+                favourites_count: Long, created_at: String, lang: String)
 
-case class User(id: Long, name: String, verifie: Boolean, followers_count: Long, friend_count: Long,
-                favourites_count: Long, created_at: String, lang: String, time_zone: String)
+case class Tweet (created_at: String, id: Long, text: String, source: String, truncated: Boolean,
+                  retweet_count: Long, favorite_count: Long, lang: String, timestamp_ms: String,
+                  user: User)
 
 case class Coordinate(longLag: Array[Long])
 

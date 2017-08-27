3aa7b25e3589122f8428d88b735e3dea2ba9dbdb
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2017-08-27 21:38:57 +0200

    [IMP] update optional fields

--- src/main/scala/org/uclm/rrc/ms/models/Tweet.scala
+++ src/main/scala/org/uclm/rrc/ms/models/Tweet.scala
@@ -1,8 +1,8 @@
 package org.uclm.rrc.ms.models
 
-case class User(id: Option[Int], id_str: Option[String], name: Option[String], verified: Option[Boolean], followers_count: Option[Long],
-                friends_count: Option[Long], favourites_count: Option[Long], created_at: Option[String],
-                lang: Option[String])
+case class User(id: Int, id_str: String, name: String, verified: Boolean, followers_count: Long,
+                friends_count: Long, favourites_count: Long, created_at: String,
+                lang: String)
 
 case class Coordinate(coordinates: Array[Double], `type`: String)
 
@@ -10,10 +10,10 @@ case class Hashtag(indices: Array[Int], text: String)
 
 case class Entity(hashtags: Option[Array[Hashtag]])
 
-case class Tweet (created_at: Option[String], id: Option[Int], id_str: Option[String], text: Option[String],
-                  source: Option[String], truncated: Option[Boolean],
-                  retweet_count: Option[Long], favorite_count: Option[Long], lang: Option[String],
-                  timestamp_ms: Option[String], user: Option[User], coordinates: Option[Coordinate], entities: Entity)
+case class Tweet (created_at: String, id: Int, id_str: String, text: String,
+                  source: String, truncated: Boolean,
+                  retweet_count: Long, favorite_count: Long, lang: String,
+                  timestamp_ms: String, user: User, coordinates: Option[Coordinate], entities: Entity)
 
 
 
